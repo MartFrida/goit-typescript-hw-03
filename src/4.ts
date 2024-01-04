d09df3196f609e4839a1f0dd947ed1a85dee1b1f
Mary <32392607+MartFrida@users.noreply.github.com>
2024-01-04 15:45:49 +0100

    did hw

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,28 @@
+class MyHouse {
+  person: Person;
+  key: Key;
+  constructor(key: Key) {}
+
+  openDoor(person: Person) {
+    return person;
+  }
+  comeIn(person: Person) {
+    if (person.getKey()) {
+      return this.person;
+    }
+  }
+}
+class Person {
+  person: string;
+  constructor(key: Key) {}
+  getKey() {
+    return person;
+  }
+}
+class Key {
+  key: number;
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +31,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
